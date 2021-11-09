93fa42fe7307b73c4a34254edecb7ff413832283
esNaglfar <60138899+esNaglfar@users.noreply.github.com>
2021-11-09 16:28:24 +0300

    Добавьте файлы проекта.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello YEAH." << endl;
+	return 0;
+}
